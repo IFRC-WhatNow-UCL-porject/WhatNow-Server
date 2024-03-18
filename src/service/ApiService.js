369b7545126c676749ebe0e05a53cefa92f1ffb3
ROWANMENG <mqy2021official@163.com>
2024-03-18 00:16:43 +0000

    fix incorrect using of dao in ApiService

--- src/service/ApiService.js
+++ src/service/ApiService.js
@@ -106,7 +106,7 @@ class ApiService {
 
     getApisByUserId = async (user_id) => {
         try {
-            var apis = await this.apiDao.findAllByWhere({user_id});
+            var apis = await this.apiDao.findByWhere({user_id});
             apis = apis.map(api => api.toJSON());
             return responseHandler.returnSuccess(httpStatus.OK, 'fetch apis by user id successfully', apis);
         } catch (e) {
