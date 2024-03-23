79283b05b272a46e019f3f8d918212d49b2a316a
ROWANMENG <mqy2021official@163.com>
2024-03-23 04:44:18 +0000

    add server

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ require('./cronJobs');
 // eslint-disable-next-line import/order
 const http = require('http');
 
+const server = http.createServer(app);
+
 server.listen(config.port, () => {
     console.log(`Server is listening to port ${config.port}`);
 });
